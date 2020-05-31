dc6c3dabf596d92ce026a22d21002404372880bb
rojasdev <57605764+rojasdev@users.noreply.github.com>
2020-05-31 14:53:43 +0800

    New upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+?>
+<?php require_once("pages/header.php"); ?>
+<div id="main-contents">
+        <?php
+            switch($page){
+                case 'gallery':
+                    require_once 'gallery.php';
+                break; 
+                case 'about':
+                    require_once 'about.php';
+                break; 
+                case 'contact':
+                    require_once 'contact.php';
+                break; 
+                default:
+                    require_once 'main.php';
+                break; 
+            }
+        ?>
+</div>
+<?php echo file_get_contents("pages/footer.php"); ?>
